1356dc07a5aedc5c10e17241dc929b22733d4ae4
KelvinQiu <kelvinqiu802@outlook.com>
2023-12-20 05:49:16 +0800

    feat: install plugin

---
+++ lua/user/plugins.lua
@@ -0,0 +1,28 @@
+-- Install Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Add Plugins
+require("lazy").setup({
+ {
+  'projekt0n/github-nvim-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('github-theme').setup({
+      -- ...
+    })
+    vim.cmd('colorscheme github_light')
+  end,
+ }  
+})
